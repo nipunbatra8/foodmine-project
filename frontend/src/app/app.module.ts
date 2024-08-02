985e4918ff6fbb947fccdf9837d55e21b8b3a27b
nipunbatra8 <83050981+nipunbatra8@users.noreply.github.com>
2024-08-02 18:53:19 -0400

    login impl

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { TitleComponent } from './components/partials/title/title.component';
 import { NotFoundComponent } from './components/partials/not-found/not-found.component';
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -31,8 +33,14 @@ import { ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ToastrModule.forRoot({
+      timeOut:3000,
+      positionClass:'toast-bottom-right',
+      newestOnTop:false
+    })
   ],
   providers: [provideHttpClient()],
   bootstrap: [AppComponent]
--- frontend/src/app/components/pages/login-page/login-page.component.ts
+++ frontend/src/app/components/pages/login-page/login-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-login-page',
@@ -10,13 +12,19 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginPageComponent implements OnInit {
   loginForm!:FormGroup;
   isSubmitted = false;
-  constructor(private formBuilder: FormBuilder) { }
+  returnUrl = '';
+  constructor(private formBuilder: FormBuilder
+    , private userService:UserService,
+     private activatedRoute:ActivatedRoute,
+     private router:Router) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       email:['', [Validators.required,Validators.email]],
       password:['', Validators.required]
-    })
+    });
+
+    this.returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'];
   }
 
   get fc(){
@@ -27,7 +35,9 @@ export class LoginPageComponent implements OnInit {
     this.isSubmitted = true;
     if(this.loginForm.invalid) return;
 
-    alert(`email: ${this.fc['email'].value},
-     password: ${this.fc['password'].value}`)
+    this.userService.login({email:this.fc['email'].value,
+      password: this.fc['password'].value}).subscribe(() => {
+        this.router.navigateByUrl(this.returnUrl);
+      });
   }
 }

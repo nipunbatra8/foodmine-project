d6de79af1693d4f8b09770bb787a77a2db8f226b
nipunbatra8 <83050981+nipunbatra8@users.noreply.github.com>
2024-08-02 18:34:23 -0400

    food-page and login

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FoodService } from '../../../services/food.service';
 import { Food } from '../../../shared/models/Food';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,15 +10,20 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  foods:Food[] = [];
-  constructor(private foodService:FoodService, activatedRoute:ActivatedRoute) {
+  foods: Food[] = [];
+  constructor(private foodService: FoodService, activatedRoute: ActivatedRoute) {
+    let foodsObservalbe:Observable<Food[]>;
     activatedRoute.params.subscribe((params) => {
-      if(params['searchTerm'])
-      this.foods = this.foodService.getAllFoodsBySearchTerm(params['searchTerm']);
+      if (params['searchTerm'])
+        foodsObservalbe = this.foodService.getAllFoodsBySearchTerm(params['searchTerm']);
       else if (params['tag'])
-        this.foods = this.foodService.getAllFoodsByTag(params['tag']);
+        foodsObservalbe = this.foodService.getAllFoodsByTag(params['tag']);
       else
-      this.foods = foodService.getAll();
+        foodsObservalbe = foodService.getAll();
+
+        foodsObservalbe.subscribe((serverFoods) => {
+          this.foods = serverFoods;
+        })
     })
   }
 

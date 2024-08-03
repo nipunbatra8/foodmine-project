cfd3a93f7f1a8b4823e50fa4aeac7beae83ba167
nipunbatra8 <83050981+nipunbatra8@users.noreply.github.com>
2024-08-02 23:34:38 -0400

    default components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -16,6 +16,10 @@ import { LoginPageComponent } from './components/pages/login-page/login-page.com
 import { ReactiveFormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { InputContainerComponent } from './components/partials/input-container/input-container.component';
+import { TextInputComponent } from './components/partials/text-input/text-input.component';
+import { InputValidationComponent } from './components/partials/input-validation/input-validation.component';
+import { DefaultButtonComponent } from './components/partials/default-button/default-button.component';
 
 @NgModule({
   declarations: [
@@ -30,6 +34,10 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     TitleComponent,
     NotFoundComponent,
     LoginPageComponent,
+    InputContainerComponent,
+    TextInputComponent,
+    InputValidationComponent,
+    DefaultButtonComponent,
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/partials/input-container/input-container.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-input-container',
+  templateUrl: './input-container.component.html',
+  styleUrl: './input-container.component.css'
+})
+export class InputContainerComponent {
+  @Input()
+  label!:string;
+  @Input()
+  bgColor = 'white';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
